fe0422e39d023b0180024936ab9f6458274f93d6
pondichys <sebastien.pondichy@gmail.com>
2023-01-28 10:40:31 +0100

    Update 01-void-base.sh

--- 01-void-base.sh
+++ 01-void-base.sh
@@ -27,8 +27,16 @@ sudo xbps-install -S socklog-void
 sudo ln -s /etc/sv/socklog-unix /var/service/
 sudo ln -s /etc/sv/nanoklogd /var/service/
 
+# Install zsh and tools
+sudo xbps-install -S zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+
 # Now some cool Terminal tools
-sudo xbps-install -S bat curl exa fd fzf jq lazygit neovim ripgrep starship stow tmux tree unzip wget zellij zoxide
+sudo xbps-install -S bat curl exa fd fzf jq neovim ripgrep starship stow tmux tree unzip wget zellij zoxide
+
+# Install & configure xdg tools
+sudo xbps-install -S xdg-user-dirs xdg-utils
+
+xdg-user-dirs-update
 
 # Install flatpak
 sudo xbps-install -S flatpak
@@ -36,3 +44,20 @@ sudo xbps-install -S flatpak
 # Configure flathub repo
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
+# Install git tools
+sudo xbps-install -S delta git github-cli lazygit
+
+# Install Docker
+sudo xbps-install docker docker-compose lazydocker
+
+# Install Kubernetes tools
+sudo xbps-install -S kubernetes-kind kubernetes-helm kubectl k9s
+
+# Install terraform and tools
+sudo xbps-install -S tflint terraform
+
+# Install python3
+sudo xbps-install -S python3 python3-pip
+
+# Install keybase
+sudo xbps-install -S kbfs keybase keybase-desktop
