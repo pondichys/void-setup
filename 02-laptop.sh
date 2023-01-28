ea63c360083b47b4a5c5fc1258c4e05aa277eb1d
pondichys <sebastien.pondichy@gmail.com>
2023-01-28 11:16:19 +0100

    Update 02-laptop.sh

--- 02-laptop.sh
+++ 02-laptop.sh
@@ -1,4 +1,8 @@
 # Some laptop specific tools
 
-# Install tlp for battery optimization
-sudo xbps-install -S tlp
+# Install and enable tlp for battery optimization
+sudo xbps-install -S tlp tlp-rdw tlpui
+sudo ln -sv /etc/sv/tlp /var/service
+
+# Install powertop for troubleshooting power consumption
+sudo xbps-install -S powertop
