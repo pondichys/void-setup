956eb3133633748dcd728af98ae171fa61cea5e0
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-05-15 10:52:06 +0200

    feat(kde plasma): add some extra packages

--- 12-kde.sh
+++ 12-kde.sh
@@ -3,4 +3,6 @@ sudo xbps-install -Sy avahi dbus elogind
 sudo ln -s /etc/sv/dbus /var/service
 sudo xbps-install -Sy kde5 kde5-baseapps
 
+# KDE plasma extras
+sudo xbps-install -Sy dolphin-plugins ffmpeg ffmpegthumbs kdeconnect kgpg plasma-wayland-protocols
 sudo xbps-install -Sy firefox
