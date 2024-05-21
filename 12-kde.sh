f1b100ae1ec7a1c8a50d39728efb565acb52cc02
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-05-21 09:45:48 +0200

    feat(kde plasma): add some packages

--- 12-kde.sh
+++ 12-kde.sh
@@ -1,8 +1,19 @@
 sudo xbps-install -Sy wayland wayland-protocols wayland-utils xorg
 sudo xbps-install -Sy avahi dbus elogind
 sudo ln -s /etc/sv/dbus /var/service
+sudo ln -s /etc/sv/avahi-daemon /var/service
 sudo xbps-install -Sy kde5 kde5-baseapps
 
 # KDE plasma extras
-sudo xbps-install -Sy dolphin-plugins ffmpeg ffmpegthumbs kdeconnect kgpg plasma-wayland-protocols
-sudo xbps-install -Sy firefox
+sudo xbps-install -Sy dolphin-plugins ffmpeg ffmpegthumbs kdeconnect kdegraphics-thumbnailers kgpg plasma-wayland-protocols Appstream
+
+# Some apps
+sudo xbps-install -Sy firefox nano neofetch
+
+# Printer support
+sudo xbps-install -Sy cups cups-filters gutenprint
+sudo xbps-install -Sy print-manager #KDE plasma printing management
+sudo ln -s /etc/sv/cupsd /var/service
+
+# Scanner support
+
