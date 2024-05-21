e0d4c5435c0edaa7cd8edce2f7ca84621aedbb63
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-05-21 17:07:07 +0200

    feat: add xfce4 install script

---
+++ 13-xfce.sh
@@ -0,0 +1,20 @@
+# Script to setup XFCE4 on Void Linux
+sudo xbps-install -Sy xorg
+
+sudo xbps-install -Sy avahi dbus elogind
+sudo ln -s /etc/sv/avahi-daemon /var/service
+sudo ln -s /etc/sv/dbus /var/service
+
+sudo xbps-install -Sy xfce4
+
+# Support for virtual filesystems
+sudo xbps-install -Sy gvfs-afc gvfs-mtp gvfs-smb
+
+sudo xbps-install -Sy firefox micro nano ufetch
+
+sudo xbps-install -Sy octoxbps
+
+sudo xbps-install -Sy cups cups-filters
+sudo ln -s /etc/sv/cupsd /var/service
+
+sudo xbps-install -Sy sane sane-airscan
