b0a0032735edd40854b8c981fef385a2b5790420
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-07-29 13:43:35 +0200

    chore: move touchpad X11 config to laptop.sh

--- 02-laptop.sh
+++ 02-laptop.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Some laptop specific tools
 
 # Install and enable tlp for battery optimization
@@ -6,3 +7,19 @@ sudo ln -sv /etc/sv/tlp /var/service
 
 # Install powertop for troubleshooting power consumption
 sudo xbps-install -Sy powertop
+
+# Setup touchpad for X11
+if [ ! -d /etc/X11/xorg.conf.d ]; then
+	sudo mkdir -p /etc/X11/xorg.conf.d
+fi
+
+if [ ! -r /etc/X11/xorg.conf.d/30-touchpad.conf ]; then
+	cat <<EOF | sudo tee -a /etc/X11/xorg.conf.d/30.touchpad.conf
+	Section "InputClass"
+	    Identifier "touchpad"
+    	Driver "libinput"
+    	MatchIsTouchpad "on"
+    	Option "Tapping" "on"
+	EndSection
+	EOF
+fi
