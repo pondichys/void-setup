7fa2893246aef512ba71077717a6c0bc165479a6
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-08-01 13:17:41 +0200

    fix: wrong aligment in touchpad.conf

--- 02-laptop.sh
+++ 02-laptop.sh
@@ -14,12 +14,12 @@ if [ ! -d /etc/X11/xorg.conf.d ]; then
 fi
 
 if [ ! -r /etc/X11/xorg.conf.d/30-touchpad.conf ]; then
-	cat <<EOF | sudo tee -a /etc/X11/xorg.conf.d/30.touchpad.conf
-	Section "InputClass"
-	    Identifier "touchpad"
-    	Driver "libinput"
-    	MatchIsTouchpad "on"
-    	Option "Tapping" "on"
-	EndSection
-	EOF
+	cat <<EOF | sudo tee /etc/X11/xorg.conf.d/30-touchpad.conf
+Section "InputClass"
+	Identifier "touchpad"
+	Driver "libinput"
+	MatchIsTouchpad "on"
+	Option "Tapping" "on"
+EndSection
+EOF
 fi
