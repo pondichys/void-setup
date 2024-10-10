9f075cf7fbb40be6422b6a91a684e3fdbcb2ab5d
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-10-10 16:51:56 +0200

    feat(sddm): change default theme to sddm-eucalyptus-drop
    This is due to the fact that SDDM theme sugar-canday has been archived
    and is no longer maintained by the owner.

--- scripts/DE/sddm-cfg.sh
+++ scripts/DE/sddm-cfg.sh
@@ -1,22 +1,25 @@
 # Keyboard layout
 # Change it if you don't use a BE keyboard
 KB=be
+theme_version="2.0.0"
 
-# Clone SDDM simplicity theme repository
-cd ~/git && git clone https://gitlab.com/isseigx/simplicity-sddm-theme.git
+# Get latest release of sddm-eucalyptus-drop
+cd ~/Downloads
+wget https://gitlab.com/api/v4/projects/37107648/packages/generic/sddm-eucalyptus-drop/${theme_version}/sddm-eucalyptus-drop-v${theme_version}.zip
 
 # Copy the simplicity folder to /usr/share/sddm/themes
-sudo cp -r ~/git/simplicity-sddm-theme/simplicity /usr/share/sddm/themes/
+sudo  unzip ~/Downloads/sddm-eucalyptus-drop-v${theme_version}.zip -d /usr/share/sddm/themes/
 
 # Create default SDDM config file
 sddm --example-config >~/sddm.conf
 
 # Set the theme to simplicity
-sed -i 's/Current=/Current=simplicity/g' ~/sddm.conf
+sed -i 's/Current=/Current=eucalyptus-drop/g' ~/sddm.conf
 
 # Move the file to /etc
-sudo mv ~/sddm.conf /etc/sddm.conf
-sudo chown root:root /etc/sddm.conf
+sudo mkdir -vp /etc/sddm.conf.d
+sudo mv ~/sddm.conf /etc/sddm.conf.d/sddm.conf
+sudo chown root:root /etc/sddm.conf.d/sddm.conf
 
 # Set keyboard layout for SDDM
 echo setxbmap "${KB}" | sudo tee -a /usr/share/sddm/scripts/Xsetup >/dev/null
