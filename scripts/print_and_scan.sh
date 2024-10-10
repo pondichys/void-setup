df3816fc6b67b366ab2bdfc39e7affd6235ca139
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2024-10-10 16:55:47 +0200

    fix(networking): add avahi and avahi-tools for network printing
    Also fixes network discovery in Thunar file manager.

--- scripts/print_and_scan.sh
+++ scripts/print_and_scan.sh
@@ -6,7 +6,8 @@ sudo xbps-install -y cups cups-filters
 sudo ln -sf /etc/sv/cupsd /var/service
 
 # Package for wifi printing
-sudo xbps-install -y nss-mdns
+sudo xbps-install -y avahi avahi-utils nss-mdns
+sudo ln -sf /etc/sv/avahi-daemon /var/service
 
 # Install SANE for scanning
 sudo xbps-install -y sane sane-airscan
