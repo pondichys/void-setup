96b737616d9766785e103d7e640573973ac748b4
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-06-23 20:06:10 +0200

    feat(print_and_scan): add system config printer installation

--- scripts/print_and_scan.sh
+++ scripts/print_and_scan.sh
@@ -9,5 +9,8 @@ sudo ln -sf /etc/sv/cupsd /var/service
 sudo xbps-install -y avahi avahi-utils nss-mdns
 sudo ln -sf /etc/sv/avahi-daemon /var/service
 
+# Package for printer configuation
+sudo xbps-install -y system-config-printer
+
 # Install SANE for scanning
 sudo xbps-install -y sane sane-airscan
