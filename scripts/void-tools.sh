d6b5737f48371df90cd55f13cf994731a3d51485
Sébastien Pondichy <sebastien.pondichy@gmail.com>
2025-06-29 09:56:26 +0200

    feat(tools): add xz to tools

--- scripts/void-tools.sh
+++ scripts/void-tools.sh
@@ -4,7 +4,7 @@
 # Run this script to install some tools
 figlet -ckf slant "Void tools"
 
-sudo xbps-install -y 7zip bat bottom chezmoi curl eza fastfetch fd fzf jq ripgrep starship tmux unzip wget yazi zoxide
+sudo xbps-install -y 7zip bat bottom chezmoi curl eza fastfetch fd fzf jq ripgrep starship tmux unzip wget xz yazi zoxide
 
 sudo xbps-install -y delta github-cli lazygit
 
